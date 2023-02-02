8b71f2c966987d8e550fe29bf0bc7489ce3753ac
Sasha Semeniuk <ssemeniuk8@nait.ca>
2023-02-02 08:14:09 -0700

    Jakarta Faces Demo Start

--- dmit2015-azure-webapp/src/main/java/dmit2015/model/CanadianIncomeTaxManager.java
+++ dmit2015-azure-webapp/src/main/java/dmit2015/model/CanadianIncomeTaxManager.java
@@ -1,25 +1,38 @@
 package dmit2015.model;
 
-import java.io.IOException;
+import lombok.Getter;
+
 import java.net.URISyntaxException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
 
 public class CanadianIncomeTaxManager {
 
-    public List<String> fetchIncomeTaxData() {
-        List<String> incomeTaxData = new ArrayList<>();
+    // Define a private constructor to implement Single pattern
+    private CanadianIncomeTaxManager() {
 
+    }
+
+    // Define a single instance of this class
+    private static CanadianIncomeTaxManager INSTANCE;
+
+    // Define a static class-level to access the singleton
+    public static CanadianIncomeTaxManager getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new CanadianIncomeTaxManager();
+        }
+        return INSTANCE;
+    }
+    @Getter
+    private List<CanadianPersonalIncomeTaxRate> incomeTaxRates;
+
+    public void loadDataFromFile() {
         try {
             Path csvPath = Path.of(getClass().getClassLoader().getResource("data/CanadianPersonalIncomeTaxRates.csv").toURI());
-            incomeTaxData = Files.readAllLines(csvPath);
-        } catch (URISyntaxException | IOException e) {
+            //incomeTaxRates = Files.readAllLines(csvPath);
+        } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
-
-        return incomeTaxData;
     }
 
 }
